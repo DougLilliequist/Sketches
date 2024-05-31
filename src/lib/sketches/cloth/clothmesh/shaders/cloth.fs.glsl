ce5c7959f621a31db7d09da33bd5f42fff80124d
Douglas Lilliequist <douglaslilliequist@Douglass-MBP.wlan>
2024-06-01 00:23:57 +0300

    tweak

--- src/lib/sketches/cloth/clothmesh/shaders/cloth.fs.glsl
+++ src/lib/sketches/cloth/clothmesh/shaders/cloth.fs.glsl
@@ -26,7 +26,7 @@ float sampleShadow(vec2 coord, float depth) {
 
     float occlusion = unpackRGBA(texture(tShadow, coord));
 //    if(occlusion < depth -  0.0001) return 0.0;
-    if(occlusion < depth -  0.00048) return 0.0;
+    if(occlusion < depth -  0.01) return 0.0;
     return 1.0;
 }
 
@@ -61,31 +61,26 @@ void main() {
     vec3 lightDir = normalize(vec3(0.0, 10.0, 5.0) - vPos);
     vec3 halfDir = normalize(eyeDir + lightDir);
 
-//    float diff = dot(lightDir, normal) * 0.5 + 0.5;
-//    float spec = pow(max(0.0, dot(halfDir, normal)), 128.0);
-
     float fresnel;
     float diff;
     float spec;
     vec3 reflectionDir;
     if(gl_FrontFacing) {
-       fresnel = pow(1.0 - max(0.0, dot(eyeDir, normal)), 2.0);
+       fresnel = pow(1.0 - max(0.0, dot(eyeDir, normal)), 1.0);
         diff = dot(lightDir, normal) * 0.5 + 0.5;
-        spec = pow(max(0.0, dot(halfDir, normal)), 256.0);
+        spec = pow(max(0.0, dot(halfDir, normal)), 128.0);
         reflectionDir = reflect(-eyeDir, normal);
     } else {
-        fresnel = pow(1.0 - max(0.0, dot(eyeDir, -normal)), 2.0);
+        fresnel = pow(1.0 - max(0.0, dot(eyeDir, -normal)), 1.0);
         diff = dot(lightDir, -normal) * 0.5 + 0.5;
-        spec = pow(max(0.0, dot(halfDir, -normal)), 256.0);
+        spec = pow(max(0.0, dot(halfDir, -normal)), 128.0);
         reflectionDir = reflect(-eyeDir, -normal);
     }
 
-
     vec3 env = texture(tEnvMap, reflectionDir).xyz;
-    vec3 col = mix(vec3(1.0, 0.25, 0.15), (diff * 0.1) + (spec * 0.3) + env * 0.6, fresnel);
     vec3 light = (diff * 0.05) + (spec * 0.2) + env * 0.7 + (normal.y * 0.5 + 0.5) * 0.05;
 
     float shadow = calcShadow(vShadowCoord);
-    FragColor = vec4(light * mix(0.1, 1.0, shadow), 1.0);
+    FragColor = vec4(light * vec3(mix(0.6, 1.0, shadow)), 1.0);
 
 }
